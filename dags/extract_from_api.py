dea0032935fd0717d3418ddbbe90007cb67c7d3e
Rthorski <alexandre.rethore@icloud.com>
2024-04-15 18:49:40 +0200

    delete extract_from_api

--- dags/extract_from_api.py
+++
@@ -1,9 +0,0 @@
-import openmeteo_requests
-import requests_cache
-import pandas as pd
-from retry_requests import retry
-
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
-
