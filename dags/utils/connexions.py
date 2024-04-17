a72489315fcd5f6d422ae753207156163a9248cb
Rthorski <alexandre.rethore@icloud.com>
2024-04-17 17:46:22 +0200

    add psql_insert_copy as a method to pd.to_sql

--- dags/utils/connexions.py
+++ dags/utils/connexions.py
@@ -1,6 +1,8 @@
 from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from google.cloud import storage
+import csv
+from io import StringIO
 
 
 config = dotenv_values('/opt/airflow/dags/.env')
@@ -21,4 +23,36 @@ def authentificateServiceAccount():
 
 storage_client = authentificateServiceAccount()
 bucket_name = 'geolocalisation_cities'
-bucket = storage_client.bucket(bucket_name)
+bucket_geolocalisation_cities = storage_client.bucket(bucket_name)
+bucket_meteo_files = storage_client.bucket('meteo_files')
+
+
+def psql_insert_copy(table, conn, keys, data_iter): #mehod
+    """
+    Execute SQL statement inserting data
+
+    Parameters
+    ----------
+    table : pandas.io.sql.SQLTable
+    conn : sqlalchemy.engine.Engine or sqlalchemy.engine.Connection
+    keys : list of str
+        Column names
+    data_iter : Iterable that iterates the values to be inserted
+    """
+    # gets a DBAPI connection that can provide a cursor
+    dbapi_conn = conn.connection
+    with dbapi_conn.cursor() as cur:
+        s_buf = StringIO()
+        writer = csv.writer(s_buf)
+        writer.writerows(data_iter)
+        s_buf.seek(0)
+
+        columns = ', '.join('"{}"'.format(k) for k in keys)
+        if table.schema:
+            table_name = '{}.{}'.format(table.schema, table.name)
+        else:
+            table_name = table.name
+
+        sql = 'COPY {} ({}) FROM STDIN WITH CSV'.format(
+            table_name, columns)
+        cur.copy_expert(sql=sql, file=s_buf)
